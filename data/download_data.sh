57a98c17900256e95e8a30676a74397e923a3cc1
Shih-Yuan Liu <syliu@mit.edu>
2016-05-18 15:01:42 -0400

    Fix measure_hmm_performance.ipynb. Add new dataset. Re-download the new pickle files use the updated download_data.sh.

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-wget https://www.dropbox.com/s/x4r2zbpvyrzvl3x/dataset_all.pickle.zip
-uzip dataset_all.pickle.zip
+# wget https://www.dropbox.com/s/x4r2zbpvyrzvl3x/dataset_all.pickle.zip
+# uzip dataset_all.pickle.zip
+wget https://www.dropbox.com/s/djnieumgalk1fhm/lane_chage_data_all_pickle.zip
+unzip lane_chage_data_all_pickle.zip
